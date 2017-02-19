43732bd8e4494798317c4eaeb79743f1a2f81981
tkadima <kadima.t@husky.neu.edu>
2017-02-19 17:38:54 -0500

    test

--- test/app.js
+++ test/app.js
@@ -6,16 +6,16 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
     console.log(process.env.MONGODB_URI);
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
-    }
+    // if(process.env.MLAB_USERNAME) {
+    //     connectionString = process.env.MLAB_USERNAME + ":" +
+    //         process.env.MLAB_PASSWORD + "@" +
+    //         process.env.MLAB_HOST + ':' +
+    //         process.env.MLAB_PORT + '/' +
+    //         process.env.MLAB_APP_NAME;
+    // }
 
     var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
+    mongoose.connect("mongo ds117859.mlab.com:17859/webdevschema -u tkadima -p truthBEAT$");
 
     var TestSchema = mongoose.Schema({
         message: String
