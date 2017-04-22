9cf789d30d490b70110c0d7e6d9049e984f105ae
tkadima <kadima.t@husky.neu.edu>
2017-04-22 19:56:43 -0400

    corrected update for doctors, api broken

--- public/project/services/search.service.client.js
+++ public/project/services/search.service.client.js
@@ -20,7 +20,7 @@
 
 
         function findAllBostonDoctors() {
-            var resource_url = "https://api.betterdoctor.com/2016-03-01/doctors?location=42.3601%2C-71.0589%2C200&sort=full-name-asc&skip=0&limit=500&user_key=3326b29298617f06799b6f3677e57396";
+            var resource_url = "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&skip=0&limit=100&user_key=3326b29298617f06799b6f3677e57396";
             return $http.get(resource_url);
             console.log(resource_url);
         }
